424e320d55cf7a060303a7b831b3e9daa4925bed
Gabriel Montalvo <gmontalvo404@gmail.com>
2019-10-21 16:20:17 -0500

    buscar y listar bicicletas funcionando

---
+++ bike-project-net-client/bike-project-net-client/bike-project-net-client/GUIDeleteBike.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bike_project_net_client
+{
+    public partial class GUIDeleteBike : Form
+    {
+        public GUIDeleteBike()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindBike_Click(object sender, EventArgs e)
+        {
+            BikeSoapService.BikeControllerClient bikeController = new BikeSoapService.BikeControllerClient();
+            BikeSoapService.bike findBike = bikeController.findBikeBySerial(txtFindSerial.Text);
+
+            if(findBike != null)
+            {
+                txtSerial.Text = findBike.serial;
+                txtBrand.Text = findBike.brand;
+                txtWeight.Text = Convert.ToString(findBike.weight);
+                txtPrice.Text = Convert.ToString(findBike.price);
+                dtpPurchaseDate.Value = findBike.purchaseDate;
+
+                if (findBike.type.Equals(BikeSoapService.type.ROAD))
+                {
+                    cbType.SelectedIndex = 0;
+                }
+                else if (findBike.type.Equals(BikeSoapService.type.MOUNTAIN))
+                {
+                    cbType.SelectedIndex = 1;
+                }
+                else
+                {
+                    cbType.SelectedIndex = 2;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bike not found");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BikeSoapService.BikeControllerClient bikeController = new BikeSoapService.BikeControllerClient();
+                String serial = txtSerial.Text;
+                bikeController.deleteBike(serial);
+                clearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClearFields_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+
+        private void clearFields()
+        {
+            txtFindSerial.Clear();
+            txtSerial.Clear();
+            cbType.SelectedIndex = 0;
+            txtBrand.Clear();
+            txtWeight.Clear();
+            txtPrice.Clear();
+            dtpPurchaseDate.Value = DateTime.Today;
+        }
+    }
+}
---
+++ bike-project-net-client/bike-project-net-client/bike-project-net-client/GuiBikeList.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows.Forms;
+
+namespace bike_project_net_client
+{
+    public partial class GuiBikeList : Form
+    {
+        WebClient webCliente;
+        public GuiBikeList()
+        {
+            InitializeComponent();
+            webCliente = new WebClient();
+            loadBikes();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadBikes();
+        }
+        private void loadBikes()
+        {
+            String uri = "http://localhost:8080/api/bike";
+            String content = webCliente.DownloadString(uri);
+            List<Bike> bikes = JsonConvert.DeserializeObject<List<Bike>>(content);
+
+            gridViewBikes.RowCount = 1;
+            foreach(Bike bike in bikes)
+            {
+                object[] fields = { bike.serial, bike.type,
+                    bike.brand, bike.weight, bike.price, bike.purchaseDate.ToShortDateString()};
+                gridViewBikes.Rows.Add(fields);
+            }
+        }
+    }
+}
