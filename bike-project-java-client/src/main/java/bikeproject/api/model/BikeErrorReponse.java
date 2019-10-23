d029f5dd214d89374cbdf4c25be83fb94378fb62
Luis Granada <luisgranada1213@gmail.com>
2019-10-22 23:37:20 -0500

    version 1.0 servicios rest

--- bike-project-java-client/src/main/java/bikeproject/api/model/BikeErrorReponse.java
+++ bike-project-java-client/src/main/java/bikeproject/api/model/BikeErrorReponse.java
@@ -3,12 +3,16 @@ package bikeproject.api.model;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Root(name = "HashMap")
 public class BikeErrorReponse {
     @ElementList(entry = "errores",inline = true)
     List<String> errores;
+    public BikeErrorReponse(){
+        errores = new ArrayList<>();
+    }
     public List<String> getErrores() {
         return errores;
     }
