447b8c5068a4cda19c0990be69e6f88b6af62f05
Luis Granda <luisgranada1213@gmail.comm>
2019-11-01 14:05:47 -0500

    Header

--- library-reactjs/src/App.js
+++ library-reactjs/src/App.js
@@ -1,25 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Header from "./components/header/Header";
-import MenuAppBar from "./components/header/MenuAppBar";
+import React from 'react';
+import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ResponsiveDrawer from './components/header/NavBar';
 function App() {
-  return (
-    <Router>
-      <div>
-        <MenuAppBar/>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+			<Router>
+				<ResponsiveDrawer>
+					<Switch>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/">
+							<Home />
+						</Route>
+					</Switch>
+				</ResponsiveDrawer>
+			</Router>
+	);
 }
-
 export default App;
---
+++ library-reactjs/src/components/header/NavBar.js
@@ -0,0 +1,152 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { MenuList, MenuItem } from '@material-ui/core';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex'
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0
+		}
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none'
+		}
+	},
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
+		width: drawerWidth
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3)
+	},
+	nested: {
+		paddingLeft: theme.spacing.unit * 4
+	}
+}));
+
+function ResponsiveDrawer(props) {
+	const { container, children } = props;
+	const classes = useStyles();
+	const theme = useTheme();
+	const [ mobileOpen, setMobileOpen ] = React.useState(false);
+
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
+	const drawer = (
+		<div>
+			<div className={classes.toolbar} />
+			<Divider />
+			<MenuList>
+				<MenuItem component={Link} to="/">
+					<ListItemText primary="Home" />
+				</MenuItem>
+			</MenuList>
+			<Divider />
+			<MenuList>
+				<MenuItem button>
+					<ListItemIcon>
+						<InboxIcon />
+					</ListItemIcon>
+					<ListItemText primary="About" />
+				</MenuItem>
+			</MenuList>
+		</div>
+	);
+
+	return (
+		<Fragment>
+			<CssBaseline />
+			<div className={classes.root}>
+				<AppBar position="fixed" className={classes.appBar}>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="start"
+							onClick={handleDrawerToggle}
+							className={classes.menuButton}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" noWrap>
+							ReactJs + Firestore App
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<nav className={classes.drawer} aria-label="mailbox folders">
+					{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+					<Hidden smUp implementation="css">
+						<Drawer
+							container={container}
+							variant="temporary"
+							anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+							open={mobileOpen}
+							onClose={handleDrawerToggle}
+							classes={{
+								paper: classes.drawerPaper
+							}}
+							ModalProps={{
+								keepMounted: true // Better open performance on mobile.
+							}}
+						>
+							{drawer}
+						</Drawer>
+					</Hidden>
+					<Hidden xsDown implementation="css">
+						<Drawer
+							classes={{
+								paper: classes.drawerPaper
+							}}
+							variant="permanent"
+							open
+						>
+							{drawer}
+						</Drawer>
+					</Hidden>
+				</nav>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					{children}
+				</main>
+			</div>
+		</Fragment>
+	);
+}
+
+ResponsiveDrawer.propTypes = {
+	/**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+	container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element)
+};
+
+export default ResponsiveDrawer;
