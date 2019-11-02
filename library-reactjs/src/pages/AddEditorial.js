e5074c74867c998a6a3e27115fa20efee990e61d
1213lego <luisgranada1213@gmail.com>
2019-11-02 03:41:17 -0500

    formulario agregar libreria

--- library-reactjs/src/pages/AddEditorial.js
+++ library-reactjs/src/pages/AddEditorial.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class AddEditorial extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        }
-        this.saveEditorial = this.saveEditorial.bind(this)
+	constructor(props) {
+		super(props);
+		this.state = {
+			country: '',
+			foundationDate: '',
+			name: '',
+			phone: ''
+		};
+		this.saveEditorial = this.saveEditorial.bind(this);
+		this.onChange = this.onChange.bind(this);
+	}
+	saveEditorial(event) {
+        event.preventDefault();
     }
-    saveEditorial(){
-
-    }
-    render() {
-        return (
-            <div>
-                <h2> Add Editorial </h2>
-            </div>
-        )
-    }
+	onChange(event) {
+		this.setState({ [event.target.name]: event.target.value });
+	}
+	render() {
+		return (
+			<div>
+				<h2> Add Editorial </h2>
+			</div>
+		);
+	}
 }
 export default AddEditorial;
--- library-reactjs/src/pages/EditorialList.js
+++ library-reactjs/src/pages/EditorialList.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
+import {withStyles} from "@material-ui/core";
+import AddEditorialDialog from "../components/addEditorial/AddEditorialDialog";
+import Firestore from "../api/FirestoreDb";
 
-class EditorialList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        }
-        this.saveEditorial = this.saveEditorial.bind(this)
-    }
-    saveEditorial(){
+const styles  = theme => ({
+	fab: {
+		margin: theme.spacing(2),
+	},
+	absolute: {
+		position: 'absolute',
+		bottom: theme.spacing(2),
+		right: theme.spacing(3),
+	},
+});
 
-    }
-    render() {
-        return (
-            <div>
-                <h2> Editorial List </h2>
-            </div>
-        )
-    }
+class EditorialList extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			open: false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	async componentDidMount() {
+		try {
+			const querySnapshot = await Firestore.getEditorials();
+			querySnapshot.forEach(function(doc) {
+				console.log(doc.id, " => ", doc.data());
+			});
+
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}
+	handleClick(){
+		this.setState({
+			open: !this.state.open
+		})
+	}
+	render(){
+		const { classes } = this.props;
+		return (
+			<div>
+				<AddEditorialDialog
+					open={this.state.open}
+					onClose={this.handleClick}
+				/>
+				<Tooltip
+					title="Add"
+					aria-label="add"
+				>
+					<Fab color="primary"
+						 className={classes.absolute}
+						 onClick={this.handleClick}
+					>
+						<AddIcon />
+					</Fab>
+				</Tooltip>
+			</div>
+		);
+	}
 }
-export default EditorialList;
+EditorialList.prototypes = {
+	classes: PropTypes.object.isRequired,
+}
+export default withStyles(styles)(EditorialList);
